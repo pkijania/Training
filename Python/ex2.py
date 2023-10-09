c3deb5806b6f2bcb120ab7994e4b57a104ca4be7
pkijania <139795239+pkijania@users.noreply.github.com>
2023-10-09 13:03:06 +0200

    Update

--- Python/ex2.py
+++ Python/ex2.py
@@ -4,8 +4,8 @@ import random
 y = str(input("heads or tails? "))
 x = random.choice(["heads","tails"])
 if(x==y):
- print('you guessed')
+    print('you guessed')
 else:
- print('you didnt guess')
+    print('you didnt guess')
 print("user: ",y)
 print("computer: ",x)
--- Python/ex5.py
+++ Python/ex5.py
@@ -4,10 +4,10 @@ list = [80,1,64,5,3,2,101,4,6,0,54,90]
 n = len(list)
 max=0
 min=0
-for i in list:
- if (min == 0 or i < min):
-   min = i
- if (max == 0 or i > max):
-   max = i
+for i in range (n):  
+  if (min == 0 or i < min):
+    min = i
+  if (max == 0 or i > max):
+    max = i
 print(min,"min")
 print(max,"max")
