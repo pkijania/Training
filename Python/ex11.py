f4e126479345ea1ef14e9d55be40b61ab13ba7bb
pkijania <139795239+pkijania@users.noreply.github.com>
2023-10-13 19:16:51 +0200

    Update ex11.py

--- Python/ex11.py
+++ Python/ex11.py
@@ -1,14 +1,7 @@
-class Lists_exercices:
+class Lists_exercises:
+    
     def __init__(self, list):
         self.list = list
-
-    # Display reversed list
-    def Reverse(self, list):
-        n = len(list)
-        new_list = []
-        for i in range(1,n+1):
-            new_list.append(list[-i])
-        return "Old list is " + str(list), "new list is " + str(new_list)
 
     # Min and max in a list
     def MinMax(self, list):
@@ -57,7 +50,18 @@ class Lists_exercices:
             if list[i] != list[n-i-1]:
                 return False
         return True
-    
+
+# Inheritance
+class Table_exercise(Lists_exercises):
+
+    # Display reversed list
+    def Reverse(self, list):
+        n = len(list)
+        new_list = []
+        for i in range(1,n+1):
+            new_list.append(list[-i])
+        return "Old list is " + str(list), "new list is " + str(new_list)
+
     # Create and display a table of 4 columns and 3 rows and count the sum of all it's elements
     def TableSum(self):
         n, m = 3, 4
@@ -78,20 +82,20 @@ class Lists_exercices:
                 sum = sum + list[i-1][j-1]
         print("Sum of elements in a table is: " + str(sum))
 
-var = Lists_exercices([1,2,3,4])
-print(var.Reverse([1,2,3,4]))
+var = Lists_exercises([1,2,3,4])
+print(var.MinMax([1,2,3,4]))
 
 print()
 
-var_2 = Lists_exercices([1,2,3,4])
-print(var_2.MinMax([1,2,3,4]))
+var_2 = Lists_exercises([1,3,2,5,4])
+print(var_2.Sort([1,3,2,5,4]))
 
 print()
 
-var_3 = Lists_exercices([1,3,2,5,4])
-print(var_3.Sort([1,3,2,5,4]))
+var3 = Table_exercise([1,2,3,4])
+print(var3.Reverse([1,2,3,4]))
 
 print()
 
-var_4 = Lists_exercices([1,2,3,4])
+var_4 = Table_exercise([1,2,3,4])
 print(var_4.TableSum())
