6be3199b544310ba80a1b5e01731f6c475748565
pkijania <139795239+pkijania@users.noreply.github.com>
2023-11-12 15:19:10 +0100

    Updated ex1

--- Selenium/Ex1/test_ex1.py
+++ Selenium/Ex1/test_ex1.py
@@ -2,35 +2,42 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Edge("D:\Programy\Selenium\Webdriver\msedgedriver.exe")
-driver.get("https://www.saucedemo.com/")
-driver.maximize_window()
+class TestPage:
 
-#logging in
-driver.find_element(By.ID, "user-name").send_keys("standard_user")
-driver.find_element(By.ID, "password").send_keys("secret_sauce")
-driver.find_element(By.ID, "login-button").click()
+    def __init__(self):
+        self.driver = webdriver.Edge("D:\Programy\Selenium\Webdriver\msedgedriver.exe")
+        self.driver.get("https://www.saucedemo.com/")
+        self.driver.maximize_window()
 
-#adding products to a cart
-driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
-driver.find_element(By.ID, "add-to-cart-sauce-labs-bike-light").click()
-driver.find_element(By.XPATH, "//span[text()='2']").click()
-driver.find_element(By.NAME, "checkout").click()
+    def logging_in(self):
+        self.driver.find_element(By.ID, "user-name").send_keys("standard_user")
+        self.driver.find_element(By.ID, "password").send_keys("secret_sauce")
+        self.driver.find_element(By.ID, "login-button").click()
 
-#typing personal data
-driver.find_element(By.NAME, "firstName").send_keys("John")
-driver.find_element(By.NAME, "lastName").send_keys("Smith")
-driver.find_element(By.NAME, "postalCode").send_keys("54-254")
+    def adding_products(self):
+        self.driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
+        self.driver.find_element(By.ID, "add-to-cart-sauce-labs-bike-light").click()
+        self.driver.find_element(By.XPATH, "//span[text()='2']").click()
+        self.driver.find_element(By.NAME, "checkout").click()
 
-#validating the purchase
-driver.find_element(By.NAME, "continue").click()
-driver.find_element(By.NAME, "finish").click()
+    def typing_data(self):
+        self.driver.find_element(By.NAME, "firstName").send_keys("John")
+        self.driver.find_element(By.NAME, "lastName").send_keys("Smith")
+        self.driver.find_element(By.NAME, "postalCode").send_keys("54-254")
 
-#logging out
-driver.find_element(By.NAME, "back-to-products").click()
-driver.find_element(By.ID, "react-burger-menu-btn").click()
-driver.implicitly_wait(3)
-driver.find_element(By.LINK_TEXT, "Logout").click()
+    def validating_purchase(self):
+        self.driver.find_element(By.NAME, "continue").click()
+        self.driver.find_element(By.NAME, "finish").click()
 
-while True:
-    pass
+    def logging_out(self):
+        self.driver.find_element(By.NAME, "back-to-products").click()
+        self.driver.find_element(By.ID, "react-burger-menu-btn").click()
+        self.driver.implicitly_wait(3)
+        self.driver.find_element(By.LINK_TEXT, "Logout").click()
+
+test = TestPage()
+test.logging_in()
+test.adding_products()
+test.typing_data()
+test.validating_purchase()
+test.logging_out()
