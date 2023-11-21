c2e805b77cc8c3a30fe171d2b2a8c59dadc0ab14
pkijania <139795239+pkijania@users.noreply.github.com>
2023-11-21 15:12:47 +0100

    Updated test_ex1.py

--- Selenium/Ex1/test_ex1.py
+++ Selenium/Ex1/test_ex1.py
@@ -6,6 +6,8 @@ import os
 class TestPage:
     def setup_class(self):
         web_driver_path = os.getenv("PATH_TO_DRIVE")
+        if(web_driver_path is None):
+            raise Exception(f"WEB_DRIVER_PATH env variable is not set. Please define it, so it points to your edge web driver")
         self.driver = webdriver.Edge(web_driver_path)
         self.driver.get("https://www.saucedemo.com/")
         self.driver.maximize_window()
