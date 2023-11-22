edff2cf5d3d3a90a07b8cdc27eae603b12fbee27
pkijania <139795239+pkijania@users.noreply.github.com>
2023-11-22 20:42:26 +0100

    Update ex1.js

--- JavaScript/ex1.js
+++ JavaScript/ex1.js
@@ -23,6 +23,32 @@ function printing(){
     console.log(1, 2, 3, 4, 5)
 }
 
+function subtractionTest(){
+    list = [];
+    let number_of_digits = parseInt(prompt("How many digits?: "), 10);
+    if (number_of_digits < 0){
+        console.log("\nNumber of digits must be at least 0")
+    }
+    else if(number_of_digits === 0){
+        console.log("\nSubtraction is:", number_of_digits)
+    }
+    else if(number_of_digits > 0){
+        console.log("Type", number_of_digits, "digit(s): ")
+        for (let i = 1; i <= number_of_digits; i++){
+            let digit = parseInt(prompt("Type a digit: "), 10);
+            list.push(digit);
+        }
+        let first_digit = list[0];
+        let sum_of_digits = 0;
+        let outcome = 0;
+        for (let i = 0; i <= list.length; i++){
+            sum_of_digits += list[i];
+            outcome = first_digit - sum_of_digits;
+        }
+        console.log("\nSubtraction is: ", outcome)
+    }
+}
+
 let choice = 0;
 while (choice != 4){
     console.log("\nMenu")
