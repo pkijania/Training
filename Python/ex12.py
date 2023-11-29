4449009842c59f5e1c6231a41bd51daccec0cf86
pkijania <139795239+pkijania@users.noreply.github.com>
2023-11-29 12:36:46 +0100

    Updated ex12.py

--- Python/ex12.py
+++ Python/ex12.py
@@ -3,23 +3,28 @@
 def calculator():
     first_digit = int(input("Type a digit: "))
     outcome = first_digit
-    while outcome != "=":
-        choice = str(input("Type an operator: (+, -, *, /, =): "))
-        match choice:
-            case "+":
-                digit = int(input("Type another digit: "))
-                outcome = outcome + digit
-            case "-":
-                digit = int(input("Type another digit: "))
-                outcome = outcome - digit
-            case "*":
-                digit = int(input("Type another digit: "))
-                outcome = outcome * digit
-            case "/":
-                digit = int(input("Type another digit: "))
-                outcome = outcome / digit
-            case "=":
-                return outcome
-                break
+    prompt_text = "Type another digit: "
+    while True:
+        choice = str(input("Type an operator (+, -, *, /, =): "))
+        choice_trimmed = choice.strip()
+        is_digit = choice.isdigit()
+        if is_digit is True:
+            print("You typed a digit, please type an operator (+, -, *, /, =): ")
+        else:
+            match choice_trimmed:
+                case "+":
+                    digit = int(input(prompt_text))
+                    outcome = outcome + digit
+                case "-":
+                    digit = int(input(prompt_text))
+                    outcome = outcome - digit
+                case "*":
+                    digit = int(input(prompt_text))
+                    outcome = outcome * digit
+                case "/":
+                    digit = int(input(prompt_text))
+                    outcome = outcome / digit
+                case "=":
+                    return outcome
 
 print("Outcome =", calculator())
